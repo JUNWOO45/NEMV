0c68956a2b1714db8552b2e079bae2ca2864defb
junwoo45 <sinyoorang@gmail.com>
2019-05-09 14:34:56 +0900

    Test vuetify and feel thrill

--- be/fe/src/views/About.vue
+++ be/fe/src/views/About.vue
@@ -1,6 +1,46 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    여기는 어바웃 페이지!
+    <h1>{{title}}</h1>
+    <v-btn :color="btnColor" @click="test">Success</v-btn>
+    <v-chip v-for="chip in chips" :color="chip.cl" @click="chipTest">{{chip.name}}</v-chip>
   </div>
 </template>
+<script>
+  export default {
+    name: 'default',
+    data() {
+      return {
+        title: 'park jun woo',
+        btnColor: 'info',
+        chips: [
+          {
+            cl: 'primary',
+            name: 'abc'
+          },
+          {
+            cl: 'red',
+            name: 'def'
+          },
+          {
+            cl: 'green',
+            name: 'ghi'
+          }
+        ]
+      }
+    },
+    methods: {
+      test() {
+        console.log("test is done!");
+        this.title='nnnn',
+        this.btnColor='warning'
+      },
+      chipTest() {
+        console.log("chip is clicked!");
+        // this.chips.name = 123;
+      }
+    }
+  }
+</script>
+<style scoped>
+</style>
