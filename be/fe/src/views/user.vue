5e92d3282e2b2a6bf5ef543e4028e038bf0d25aa
junwoo45 <sinyoorang@gmail.com>
2019-05-11 14:18:26 +0900

    Practice UI components. alerts, avatars,badges,buttons...

--- be/fe/src/views/user.vue
+++ be/fe/src/views/user.vue
@@ -1,8 +1,59 @@
 <template>
-    <v-container grid-list-md text-xs-center>
-        <v-layout row wrap>
-            <v-flex xs12>
-            </v-flex>
-        </v-layout>
-    </v-container>
+    <v-card height="200px" flat>
+        <div class="headline text-xs-center pa-5">
+        Active: {{ bottomNav }}
+            <v-btn @click="plus" dark>+</v-btn>
+            <v-btn @click="minus" dark>-</v-btn>
+        </div>
+        <v-bottom-nav
+        :value="true"
+        absolute
+        color="transparent"
+        >
+            <v-btn
+                color="teal"
+                flat
+                value="recent"
+            >
+                <span>junwoo</span>
+                <v-icon>history</v-icon>
+            </v-btn>
+
+            <v-btn
+                color="teal"
+                flat
+                value="favorites"
+            >
+                <span>gaon</span>
+                <v-icon>favorite</v-icon>
+            </v-btn>
+
+            <v-btn
+                color="teal"
+                flat
+                value="nearby"
+            >
+                <span>bless</span>
+                <v-icon>place</v-icon>
+            </v-btn>
+        </v-bottom-nav>
+    </v-card>
 </template>
+
+<script>
+    export default {
+        data () {
+            return {
+                bottomNav: 0
+            }
+        },
+        methods: {
+            plus() {
+                this.bottomNav = this.bottomNav + 1
+            },
+            minus() {
+                this.bottomNav = this.bottomNav - 1
+            }
+        }
+  }
+</script>
