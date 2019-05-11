4631e25534bafa1dfbf7608e239aa898e9056337
Parkjunwoo <sinyoorang@gmail.com>
2019-05-11 18:45:56 +0900

    Practice vuetify ui component

--- be/fe/src/views/user.vue
+++ be/fe/src/views/user.vue
@@ -1,50 +1,60 @@
 <template>
-    <v-card height="200px" flat>
-        <div class="headline text-xs-center pa-5">
-        Active: {{ bottomNav }}
-            <v-btn @click="plus" dark>+</v-btn>
-            <v-btn @click="minus" dark>-</v-btn>
+    <v-hover>
+    <v-card
+      slot-scope="{ hover }"
+      :class="`elevation-${hover ? 12 : 2}`"
+      class="mx-auto"
+      width="344"
+    >
+      <v-img
+        :aspect-ratio="16/9"
+        src="https://cdn.vuetifyjs.com/images/cards/cooking.png"
+      ></v-img>
+      <v-card-title>
+        <div>
+          <span class="headline">Cafe Badilico</span>
+          <div class="d-flex">
+            <v-rating
+              :value="value"
+              color="amber"
+              dense
+              half-increments
+              readonly
+              size="14"
+            ></v-rating>
+            <div class="ml-2 grey--text text--darken-2">
+              <span>{{ value }}</span>
+              <span>({{ reviews }})</span>
+            </div>
+          </div>
         </div>
-        <v-bottom-nav
-        :value="true"
-        absolute
-        color="transparent"
-        >
-            <v-btn
-                color="teal"
-                flat
-                value="recent"
-            >
-                <span>junwoo</span>
-                <v-icon>history</v-icon>
-            </v-btn>
-
-            <v-btn
-                color="teal"
-                flat
-                value="favorites"
-            >
-                <span>gaon</span>
-                <v-icon>favorite</v-icon>
-            </v-btn>
-
-            <v-btn
-                color="teal"
-                flat
-                value="nearby"
-            >
-                <span>bless</span>
-                <v-icon>place</v-icon>
-            </v-btn>
-        </v-bottom-nav>
+        <v-spacer></v-spacer>
+        <v-btn icon class="mr-0">
+          <v-icon>mdi-chevron-right</v-icon>
+        </v-btn>
+      </v-card-title>
     </v-card>
+  </v-hover>
 </template>
 
 <script>
     export default {
         data () {
             return {
-                bottomNav: 0
+                lists: [
+                    {
+                        title: "강릉가는길",
+                        content: "우리는 지금 강릉간다"
+                    },
+                    {
+                        title: "가온이가 주도했다.",
+                        content: "대단한 임산부다..!"
+                    },
+                    {
+                        title: "축복이도 함께간다.",
+                        content: "3명의 두번째 여행이다."
+                    },
+                ]
             }
         },
         methods: {
@@ -53,6 +63,9 @@
             },
             minus() {
                 this.bottomNav = this.bottomNav - 1
+            },
+            btnClicked() {
+                this.cycleTest = !this.cycleTest
             }
         }
   }
