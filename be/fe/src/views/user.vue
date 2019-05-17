89d0cc6784889acbafaeba12d05498b85f13f2b9
junwoo45 <sinyoorang@gmail.com>
2019-05-17 10:54:49 +0900

    Upload

--- be/fe/src/views/user.vue
+++ be/fe/src/views/user.vue
@@ -2,24 +2,8 @@
     <v-container grid-list-md text-xs-center>
         <v-layout row wrap>
             <v-flex xs12 v-for="u in users">
-                <v-card>
-                    <v-img
-                    src="https://cdn.vuetifyjs.com/images/cards/desert.jpg"
-                    aspect-ratio="2.75"
-                    ></v-img>
-
-                    <v-card-title primary-title>
-                    <div>
-                        <h3 class="headline mb-0">{{u.name}}</h3>
-                        <div> {{ card_text }} </div>
-                    </div>
-                    </v-card-title>
-
-                    <v-card-actions>
-                    <v-btn flat color="orange">{{u.age}}</v-btn>
-                    <v-btn flat color="orange">Explore</v-btn>
-                    </v-card-actions>
-                </v-card>
+                <v-chip close>{{u.name}}</v-chip>
+                <v-chip close>{{u.age}}</v-chip>
             </v-flex>
         </v-layout>
     </v-container>
--- be/routes/api/user/index.js
+++ be/routes/api/user/index.js
@@ -2,19 +2,31 @@ var express = require('express');
 var createError = require('http-errors');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  const us = [
-    {
-      name: 'park',
-      age: 10
-    },
-    {
-      name: 'lee',
-      age: 9
-    }
-  ];
+const us = [
+  {
+    name: 'park',
+    age: 10
+  },
+  {
+    name: 'lee',
+    age: 9
+  }
+];
 
+router.get('/', function(req, res, next) {
   res.send({users: us});
+});
+
+router.post('/', function(req, res, next) {
+  res.send({success: true});
+});
+
+router.put('/', function(req, res, next) {
+  res.send({success: true});
+});
+
+router.delete('/', function(req, res, next) {
+  res.send({success: true});
 });
 
 router.all('*', function(req, res, next) {
