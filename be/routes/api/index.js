cd2c87c202762e796e15e60a92bc9140f53c6754
Parkjunwoo <sinyoorang@gmail.com>
2019-05-28 23:20:14 +0900

    미들웨어 연습

--- be/routes/api/index.js
+++ be/routes/api/index.js
@@ -1,10 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var createError = require('http-errors');
 
-router.get('/', function(req, res, next) {
-  res.send('나는 어떤 api입니다.');
+router.all('*', function(req, res, next) {
+  console.log(req.headers);
+  console.log("req.path : ", req.path);
+  if(req.path === '/xxx') {
+    return res.send({연습:"성공"})
+  }
+  next();
 });
 
 router.use('/user', require('./user'));
+
+router.all('*', function(req, res, next) {
+  next(createError(404, '그런 api 없음요!!!!!'));
+});
 
 module.exports = router;
